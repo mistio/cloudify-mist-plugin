c9600cd6c0e296c8f4b9b9cf68df44ff86052a46
vasilis <vasilis@peerbay.net>
2015-09-24 16:04:57 +0300

    Testing

--- plugin/server.py
+++ plugin/server.py
@@ -189,7 +189,7 @@ def delete(**_):
 # def creation_validation(nova_client, args, **kwargs):
 @operation
 def run_script(**kwargs):
-    print "scriptttttttttt:", kwargs
+    print "scriptttttttttt:", kwargs["ctx"].node.properties
     client = connection.MistConnectionClient().client
     script = kwargs.get('script', '')
     name = kwargs.get("name", '')
