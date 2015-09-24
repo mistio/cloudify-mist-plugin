1069f5bc0284d45f6ebc61c875834065ebcbee30
vasilis <vasilis@peerbay.net>
2015-09-24 16:26:07 +0300

    Changed version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-mist-plugin',
-    version='0.0.1',
+    version='0.0.2',
     author='mist',
     author_email='info@mist.io',
     description='Cloudify plugin for Mist infrastructure.',
