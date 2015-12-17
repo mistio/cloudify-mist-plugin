882546e486e33d2daabd1d49ae595a2e12916109
vasilis <vasilis@mist.io>
2015-12-17 20:48:15 +0200

    add github latest mist.client

--- setup.py
+++ setup.py
@@ -13,8 +13,10 @@ setup(
     install_requires=[
         # Necessary dependency for developing plugins, do not remove!
         'cloudify-plugins-common>=3.3a5',
-        'mist',
     ],
+    dependency_links=[
+        'https://github.com/mistio/mist.client/archive/master.zip',
+    ]
     test_requires=[
         'cloudify-dsl-parser>=3.3a5',
         'nose',
