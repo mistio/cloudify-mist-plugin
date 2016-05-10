2e499b45debef2ffed28ebdbcabcc3266900d28d
vasilis Kef <vasilis@mist.io>
2016-05-10 18:42:10 +0300

    simplify code to use mist.client to validate requests and throw errors

--- plugin/server.py
+++ plugin/server.py
@@ -90,69 +90,13 @@ def create(**_):
     cloud = mist_client.cloud
     if ctx.node.properties['use_external_resource']:
         machine = mist_client.machine
-        ctx.instance.runtime_properties['ip'] = machine.info["public_ips"][0]
-        ctx.instance.runtime_properties['networks'] = {
-            "default": machine.info["public_ips"][0]}
-        ctx.instance.runtime_properties['machine_id'] = machine.info["id"]
-        ctx.instance.runtime_properties['cloud_id'] = cloud.id
-        ctx.instance.runtime_properties['mist_type'] = "machine"
-        ctx.logger.info('External machine attached to ctx')
+        ctx.instance.runtime_properties = machine.info
         return
-    machines = cloud.machines(
-        search=ctx.node.properties['parameters']["name"])
-    if len(machines):
-        for m in machines:
-            if m.info["state"] in ["running", "stopped"]:
-                raise NonRecoverableError(
-                    "Machine with name {0} exists".format(ctx.node.properties['parameters']["name"]))
-
-    key = ""
-    if ctx.node.properties['parameters'].get("key_name"):
-        key = client.keys(search=ctx.node.properties['parameters']["key_name"])
-        if len(key):
-            key = key[0]
-        else:
-
-            raise NonRecoverableError("key not found")
-    else:
-        raise NonRecoverableError("key not found")
-    # print 'Key:', key
-    if ctx.node.properties['parameters'].get("networks"):
-        networks = ctx.node.properties['parameters']["networks"]
-    else:
-        networks = []
-    ctx.logger.info('Networks are: {0}'.format(networks))
-    job_id = cloud.create_machine(async=True, name=ctx.node.properties['parameters']["name"],
-                                  key=key, image_id=ctx.node.properties[
-                                      'parameters']["image_id"],
-                                  location_id=ctx.node.properties[
-                                      'parameters']["location_id"],
-                                  size_id=ctx.node.properties[
-                                      'parameters']["size_id"],
-                                  networks=networks)
-    job_id = job_id.json()["job_id"]
-    job = client.get_job(job_id)
-    timer = 0
-    while True:
-        if job["summary"]["probe"]["success"]:
-            break
-        if job["summary"]["create"]["error"] or job["summary"]["probe"]["error"]:
-            ctx.logger.error('Error on machine creation:{0}'.format(job))
-            raise NonRecoverableError("Not able to create machine")
-        sleep(10)
-        job = client.get_job(job_id)
-        timer += 1
-        if timer >= 360:   # timeout 1hour
-            raise NonRecoverableError("Timeout.Not able to create machine.")
-
-    # print job["summary"]
+    try:
+        cloud.create_machine(async=True, **ctx.node.properties['parameters'])
+    except Exception as exc:
+        raise NonRecoverableError(exc)
     machine = mist_client.machine
-    ctx.instance.runtime_properties['machine_id'] = machine.info["id"]
-    ctx.instance.runtime_properties['cloud_id'] = cloud.id
-    ctx.instance.runtime_properties['mist_type'] = "machine"
-    ctx.instance.runtime_properties['ip'] = machine.info["public_ips"][0]
-    ctx.instance.runtime_properties['networks'] = {
-        "default": machine.info["public_ips"][0]}
     ctx.logger.info('Machine created')
 
 
@@ -160,25 +104,35 @@ def create(**_):
 def start(**_):
     try:
         connection.MistConnectionClient().machine.start()
-        ctx.logger.info('Machine started')
-    except:
-        ctx.logger.info("Failed to start machine")
-        print connection.MistConnectionClient().machine.info
-    if ctx.node.properties.get("monitoring"):
-        connection.MistConnectionClient().machine.enable_monitoring()
-        ctx.logger.info('Monitoring enabled')
+    except Exception as exc:
+        raise Exception(exc)
+        # ctx.logger.info("Failed to start machine")
+        # print connection.MistConnectionClient().machine.info
+    # if ctx.node.properties.get("monitoring"):
+    #     connection.MistConnectionClient().machine.enable_monitoring()
+    #     ctx.logger.info('Monitoring enabled')
 
 
 @operation
 def stop(**_):
-    connection.MistConnectionClient().machine.stop()
-    ctx.logger.info('Machine stopped')
+
+    try:
+        connection.MistConnectionClient().machine.stop()
+    except Exception as exc:
+        raise Exception(exc)
+    # connection.MistConnectionClient().machine.stop()
+    # ctx.logger.info('Machine stopped')
 
 
 @operation
 def delete(**_):
-    connection.MistConnectionClient().machine.destroy()
-    ctx.logger.info('Machine destroyed')
+
+    try:
+        connection.MistConnectionClient().machine.destroy()
+    except Exception as exc:
+        raise Exception(exc)
+    # connection.MistConnectionClient().machine.destroy()
+    # ctx.logger.info('Machine destroyed')
 
 
 @operation
@@ -188,71 +142,22 @@ def run_script(**kwargs):
     name = kwargs.get("name", '')
     scripts = client.get_scripts()
     machine = connection.MistConnectionClient().machine
+    script_params = kwargs.get("params","")
     if kwargs.get("script_id", ''):
         script_id = kwargs["script_id"]
-        job_id = client.run_script(script_id=script_id,
-                                   cloud_id=ctx.node.properties['parameters'][
-                                       'cloud_id'],
-                                   machine_id=ctx.instance.runtime_properties[
-                                       'machine_id'],
-                                   script_params=kwargs.get("params", ""))
+        try:
+            job_id = machine.run_script(**kwargs)
+        except Exception as exc:
+            raise NonRecoverableError(exc)
+
     else:
-        if kwargs.get("exec_type", ''):
-            exec_type = kwargs["exec_type"]
-        else:
-            exec_type = "executable"
-
-        if kwargs.get("location_type", ""):
-            location_type = kwargs["location_type"]
-        else:
-            if script.startswith('http://github.com') or script.startswith('https://github.com'):
-                location_type = 'github'
-            elif (script.startswith('http://') or script.startswith('https://')):
-                location_type = 'url'
-            elif kwargs.get("tmp_prefix"):
-                script = kwargs["tmp_prefix"] + script
-                if os.path.exists():
-                    if not name:
-                        name = script.split("/").pop()
-                    location_type = 'inline'
-                    with open(script, "r") as scriptfile:
-                        script = scriptfile.read()
-            elif os.path.exists(script):
-                if not name:
-                    name = script.split("/").pop()
-                location_type = 'inline'
-                with open(script, "r") as scriptfile:
-                    script = scriptfile.read()
-            elif script.startswith("#!"):
-                location_type = 'inline'
-            else:
-                raise NonRecoverableError(
-                    "Script not found {0}".format(script))
-        if not name:
-            uid = ''.join(
-                random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
-            name = ctx.node.properties["parameters"]["name"] + uid
-
-        for s in scripts:
-            if s['name'] == name:
-                raise NonRecoverableError("Script with name {0} exists. Rename the script \
-                                            or use external resource.".format(name))
-        response = client.add_script(
-            name=name, script=script, location_type=location_type, exec_type=exec_type)
         script_id = response['script_id']
-        job_id = client.run_script(script_id=script_id, cloud_id=ctx.node.properties['parameters']['cloud_id'],
-                                   machine_id=ctx.instance.runtime_properties['machine_id'], script_params=kwargs.get("params", ""))
-    ctx.logger.info("Script with name {0} started".format(name))
-    job_id = job_id["job_id"]
-    job = client.get_job(job_id)
-    while True:
-        if job["finished_at"]:
-            break
-        if job["error"]:
-            raise NonRecoverableError(
-                "Not able to run script {0}".format(name))
-        sleep(10)
-        job = client.get_job(job_id)
-    ctx.logger.info(job["logs"][2]['stdout'])
-    ctx.logger.info(job["logs"][2]['extra_output'])
-    ctx.logger.info("Script with name {0} succeeded".format(name))
+        try:
+            response = client.add_and_run_script(machine.cloud.id, machine.id,
+                                                 script_params=script_params,
+                                                 fire_and_forget=False,
+                                                 **kwargs)
+            # machine.run_script(script_id=script_id, script_params=script_params,
+            #                    fire_and_forget=False)
+        except Exception as exc:
+            raise NonRecoverableError(exc)
