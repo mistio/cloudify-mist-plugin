9efd7701e431a4261146b8d12e32e60ce1bcbe23
vasilis Kef <vasilis@mist.io>
2016-05-18 19:14:49 +0300

    add network node_type and tests for network

--- plugin/tests/test_plugin.py
+++ plugin/tests/test_plugin.py
@@ -12,11 +12,11 @@ class TestPlugin(unittest.TestCase):
                                                    'test_plugin.yaml')],
                    inputs={'api_token': "df94cf8ca00e8f9b4d3873c4c9bd35f6854783438e66478819c8b7f91c5c4a37",
                            'mist_uri': "http://172.17.0.1",
-                           'cloud_id': "492f3ad72ebe4b27b21f83b93e96b2d1",
+                           'cloud_id': "c204bf73f65544b1b63c83b16373660a",
                            'key_id': "hello2",
-                           'image_id': "ami-a21529cc",
-                           'size_id': "t1.micro",
-                           'location_id': "ap-northeast-1a",
+                           'image_id': "d69e1f0e-5205-4698-880e-81f95774a633",
+                           'size_id': "1",
+                           'disk': "1",
                            'machine_name': "happesd"
 
                            }
@@ -30,15 +30,15 @@ class TestPlugin(unittest.TestCase):
         for instance in cfy_local.storage.get_node_instances():
             prop = instance.runtime_properties
             if prop.get("mist_type") == "machine":
+                assert prop.get('ip'), "No ip set in runtime properties."
+                assert prop.get('networks'), "No network set in runtime properties."
                 break
         # instance = cfy_local.storage.get_node_instances()[1]
         # assert runtime properties is properly set in node instance
-        print(prop)
+        # print(prop)
 
-        assert prop.get('ip'), "No ip set in runtime properties."
-        assert prop.get('networks'), "No network set in runtime properties."
         # assert prop.get('private_key_path'), "No private key path set in runtime properties."
         """Test uninstall workflow"""
-        cfy_local.execute('uninstall', task_retries=10)
+        # cfy_local.execute('uninstall', task_retries=10)
 
         # assert False
