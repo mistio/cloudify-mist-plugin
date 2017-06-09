8b5328697f5ead1eefd2d6b511d445241993da71
Christos Pollalis <pollalis.ch@gmail.com>
2017-06-09 20:24:32 +0300

    Minor updates to setup.py

--- setup.py
+++ setup.py
@@ -3,22 +3,26 @@ from setuptools import setup
 
 setup(
     name='cloudify-mist-plugin',
+
     version='0.0.10',
-    author='mist',
-    author_email='info@mist.io',
-    description='Cloudify plugin for Mist infrastructure.',
+    author='mist.io',
+    author_email='support@mist.io',
+    description='A Cloudify Plugin that provisions resources across IaaS '
+                'Cloud Providers through Mist.io',
+
     packages=['plugin'],
+
     license='LICENSE',
     zip_safe=False,
     install_requires=[
         # Necessary dependency for developing plugins, do not remove!
-        'cloudify-plugins-common==3.3.1',
+        'cloudify-plugins-common==3.3',
     ],
     dependency_links=[
         'https://github.com/mistio/mist.client/archive/master.zip',
     ],
     test_requires=[
-        'cloudify-dsl-parser==3.3.1',
+        'cloudify-dsl-parser==3.3',
         'nose',
     ]
 )
