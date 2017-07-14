c4ec75961460eabc0c3ce3bf359f082868a6c1fe
Kostis Tsaprailis <kostis.tsaprailis@gmail.com>
2017-07-14 13:47:16 +0300

    fixing server run_script params

--- plugin/server.py
+++ plugin/server.py
@@ -182,7 +182,7 @@ def run_script(**kwargs):
     client = connection.MistConnectionClient().client
     machine = connection.MistConnectionClient().machine
     kwargs['cloud_id'] = machine.cloud.id
-    script_params = kwargs.get("params", "")
+    script_params = kwargs.pop("params", "")
     kwargs.pop('ctx', None)
     print "kwargs: %s" % kwargs
     print "done"
