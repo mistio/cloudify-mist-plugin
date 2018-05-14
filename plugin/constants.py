2c227eb02dc2139a409adcc4351b193b8f0da994
Christos Pollalis <pollalis.ch@gmail.com>
2018-05-15 01:31:51 +0300

    Created a new version of the LocalStorage
    The old LocalStorage has been deprecated and it's to be removed,
    once the kubernetes blueprint's scale_down workflow stops using it.

--- plugin/constants.py
+++ plugin/constants.py
@@ -10,6 +10,7 @@ INSTANCE_REQUIRED_PROPERTIES = [
 ]
 
 STORAGE = 'local-storage/local/node-instances/%s_[A-Za-z0-9]*'
+STORAGE2 = '/tmp/templates/mistio-kubernetes-blueprint-[A-Za-z0-9]*/local-storage/local/node-instances'
 
 CREATE_TIMEOUT = 60 * 10
 SCRIPT_TIMEOUT = 60 * 30
